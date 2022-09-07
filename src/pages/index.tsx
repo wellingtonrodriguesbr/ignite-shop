93bbbe93b15aefc7d82c04503a923c93e66819b9
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-09-07 10:13:56 -0300

    chore: WIP global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,13 @@
+import { styled } from "../styles";
+
+const Button = styled("button", {
+  background: "$rocketseat",
+  color: "#fff",
+  padding: "8px 16px",
+  border: "none",
+  borderRadius: 4,
+});
+
 export default function Home() {
-  return <h1>Hello World!</h1>;
+  return <Button>Hello World!</Button>;
 }
